343b2c9532690ecc75a3ae1d511d20a37f3ed19d
Jackieriel <jackieriel14@gmail.com>
2020-07-31 18:48:17 +0100

    Procfile change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'theAs20') }}  #theA's - @yield('title')</title>
+    <title>{{ config('app.name', 'theAs20') }} - @yield('title')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/main.js') }}" defer></script>
