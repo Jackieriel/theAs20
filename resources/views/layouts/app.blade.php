49d06e482372d9c947d43697275dcea61492ef62
Jackieriel <jackieriel14@gmail.com>
2020-09-03 12:42:53 +0100

    Added google tool

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="k8JbKK60eu-5Pr6ZvUsHUJ54JY9Wy4JNFFhpGjOvM8s" />
 
     <title>{{ config('app.name', 'theAs20') }} theAs20 - @yield('title')</title>
 
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="k8JbKK60eu-5Pr6ZvUsHUJ54JY9Wy4JNFFhpGjOvM8s" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
